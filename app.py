d80712c07c5ca6f05ecbd3fb856c1a64aee51bfa
Dan Nguyen <dansonguyen@gmail.com>
2017-06-09 17:03:03 -0700

    stuff

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from foo.getdata import get_incidents
+from foo.getdata import get_incidents, get_districts
 
 app = Flask(__name__)
 
@@ -9,13 +9,20 @@ def hello():
 
 @app.route("/")
 def homepage():
-    return render_template("index.html", incidents=get_incidents())
+    return render_template("index.html", incidents=get_incidents(), districts=get_districts())
 
 
 @app.route("/year/<year>")
 def yearpage(year):
     incidents = [i for i in get_incidents() if i['year'] == year]
     return render_template("yearpage.html", incidents=incidents, year=year)
+
+@app.route("/pd/<pname>")
+def precinctpage(pname):
+    pname = pname.upper()
+    incidents = [i for i in get_incidents() if i['PdDistrict'] == pname]
+    return render_template("pdpage.html", incidents=incidents, district_name=pname)
+
 
 
 
--- foo/getdata.py
+++ foo/getdata.py
@@ -1,5 +1,6 @@
 from os.path import join
 import csv
+from collections import Counter
 
 DATA_DIR = join('static', 'data')
 INCIDENTS_FNAME = join(DATA_DIR, 'incidents.csv')
@@ -9,3 +10,11 @@ def get_incidents():
     with open(INCIDENTS_FNAME) as f:
         data = list(csv.DictReader(f))
     return data
+
+
+def get_districts():
+    names = []
+    for i in get_incidents():
+        names.append(i['PdDistrict'])
+
+    return sorted(Counter(names).items())
